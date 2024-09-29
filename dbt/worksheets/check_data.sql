c8f359ef048afe1d509a4ebe746dfecd68954ccd
niloofar <mousavi.n0@gmail.com>
2024-09-29 12:23:25 +0200

    dbt works3

---
+++ dbt/worksheets/check_data.sql
@@ -0,0 +1,5 @@
+use ROLE job_ads_dbt_role;
+use database job_ads;
+use schema staging;
+
+show views;
--- dbt/worksheets/create_role.sql
+++ dbt/worksheets/create_role.sql
@@ -5,3 +5,5 @@ USE ROLE USERADMIN;
 CREATE ROLE job_ads_dbt_role;
 
 GRANT ROLE job_ads_dbt_role TO USER niloofarmoosavi;
+
+
