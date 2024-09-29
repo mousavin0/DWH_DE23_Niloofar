1755b19944e157ac3817eef308739a8bf3a493e9
niloofar <mousavi.n0@gmail.com>
2024-09-29 14:12:33 +0200

    updated fact and mart, use auxiliary

--- dbt/dbt_code/models/fct/fct_job_ads.sql
+++ dbt/dbt_code/models/fct/fct_job_ads.sql
@@ -1,24 +1,34 @@
-WITH ja AS (SELECT * FROM {{ ref('src_job_ads') }}),
 
-jd AS (SELECT * FROM {{ ref('src_job_details') }}),
 
-e AS (SELECT * FROM {{ ref('src_employer') }})
+WITH ja AS (
+    SELECT * FROM {{ ref('src_job_ads') }}
+),
+
+jd AS (
+    SELECT * FROM {{ ref('src_job_details') }}
+),
+
+e AS (
+    SELECT * FROM {{ ref('src_employer') }}
+),
+
+aux AS (
+    SELECT * FROM {{ ref('src_auxiliary') }} -- Reference the auxiliary dimension
+)
 
 SELECT 
-    {{dbt_utils.generate_surrogate_key(['jd.id', 'jd.headline'])}} as job_details_key,
-    {{dbt_utils.generate_surrogate_key(['e.id', 'e.employer_name'])}} as employer_key,
-    {# TODO: key to junk dimension #}
-    relevance, -- if null, aggregation functions can handle it properly
-    coalesce(vacancies, 1) as vacancies,
-    application_deadline
-    -- for verifying FK relationships work
-    {# jd.id,
-    e.workplace_city,
-    e.employer_name,
-    jd.description #}
+    ja.id AS job_id,
+    {{ dbt_utils.generate_surrogate_key(['jd.id', 'jd.headline']) }} AS job_details_key, -- Key for job details
+    {{ dbt_utils.generate_surrogate_key(['e.id', 'e.employer_name']) }} AS employer_key, -- Key for employer
+    {{ dbt_utils.generate_surrogate_key(['aux.experience_required', 'aux.driving_license_required']) }} AS auxiliary_key, -- Key for auxiliary attributes
+    coalesce(ja.vacancies, 1) AS vacancies,
+    coalesce(ja.relevance, 0) AS relevance,
+    coalesce(ja.application_deadline, current_date) AS application_deadline -- Default to current date if null
 FROM 
     ja 
 LEFT JOIN 
     jd ON ja.id = jd.id
 LEFT JOIN 
-    e ON ja.id = e.id
+    e ON ja.id = e.id -- Ensure to join by the correct foreign key
+LEFT JOIN 
+    aux ON ja.id = aux.id -- Join auxiliary table
--- dbt/dbt_code/models/mart/mart_job_listings.sql
+++ dbt/dbt_code/models/mart/mart_job_listings.sql
@@ -2,25 +2,37 @@ WITH fct_job_ads AS (
     SELECT * FROM {{ ref('fct_job_ads') }}
 ),
 
-job_details AS (SELECT * FROM {{ ref('dim_job_details') }}),
+job_details AS (
+    SELECT * FROM {{ ref('dim_job_details') }}
+),
 
-employer AS (SELECT * FROM {{ ref('dim_employer') }})
+employer AS (
+    SELECT * FROM {{ ref('dim_employer') }}
+),
+
+auxiliary AS (
+    SELECT * FROM {{ ref('dim_auxiliary') }}
+)
 
 SELECT
-    jd.headline,
-    f.vacancies,
-    jd.salary_type,
-    f.relevance,
-    e.employer_name,
-    e.workplace_city,
-    jd.description,
-    jd.description_html_formatted,
-    jd.duration,
-    jd.scope_of_work_min,
-    jd.scope_of_work_max,
-    f.application_deadline
+    jd.headline,                          -- Job headline
+    f.vacancies,                          -- Number of vacancies
+    jd.salary_type,                       -- Salary type
+    f.relevance,                          -- Job relevance
+    e.employer_name,                      -- Employer name
+    e.workplace_city,                     -- Workplace city
+    jd.description,                       -- Job description
+    jd.description_html_formatted,        -- HTML formatted description
+    jd.duration,                          -- Job duration
+    jd.scope_of_work_min,                 -- Minimum scope of work
+    jd.scope_of_work_max,                 -- Maximum scope of work
+    f.application_deadline,               -- Application deadline
+    aux.experience_required,              -- Experience required (from auxiliary dimension)
+    aux.driving_license_required          -- Driving license required (from auxiliary dimension)
 FROM fct_job_ads AS f
 LEFT JOIN job_details AS jd
     ON f.job_details_key = jd.job_details_id
 LEFT JOIN employer AS e
     ON f.employer_key = e.employer_id
+LEFT JOIN auxiliary AS aux
+    ON f.auxiliary_key = aux.auxiliary_id
