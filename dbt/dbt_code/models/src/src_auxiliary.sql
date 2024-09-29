9445eb748addb2ae6c93bd63fb209c013bc78668
niloofar <mousavi.n0@gmail.com>
2024-09-29 14:10:50 +0200

    add src auxiliary

---
+++ dbt/dbt_code/models/src/src_auxiliary.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    materialized = 'ephemeral',
+  )
+}}
+
+WITH stg_job_ads AS (
+    SELECT * FROM {{ source('job_ads', 'stg_data_ads') }}
+)
+
+SELECT
+    id,
+    coalesce(experience_required, 'Not specified') AS experience_required, -- Fill nulls
+    coalesce(driving_license_required, 'Nos') AS driving_license_required -- Fill nulls
+FROM stg_job_ads
