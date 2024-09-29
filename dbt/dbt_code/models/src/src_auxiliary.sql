ab49a0a67c54ad5c2a289d9747e1cc00906b4bd4
niloofar <mousavi.n0@gmail.com>
2024-09-29 15:19:32 +0200

    edited null value

--- dbt/dbt_code/models/src/src_auxiliary.sql
+++ dbt/dbt_code/models/src/src_auxiliary.sql
@@ -11,5 +11,5 @@ WITH stg_job_ads AS (
 SELECT
     id,
     coalesce(experience_required, 'Not specified') AS experience_required, -- Fill nulls
-    coalesce(driving_license_required, 'Nos') AS driving_license_required -- Fill nulls
+    coalesce(driving_license_required, 'Not specified') AS driving_license_required -- Fill nulls
 FROM stg_job_ads
